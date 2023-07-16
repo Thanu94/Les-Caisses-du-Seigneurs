14700092792a8784c4110c6e6d37840bd86e3e0d
Thanujan THAVARATNAM <84854187+Thanu94@users.noreply.github.com>
2023-07-16 01:51:01 +0100

    feat: Login module with database

--- src/Admin/Login/acces.php
+++ src/Admin/Login/acces.php
@@ -1,24 +1,50 @@
 <?php
-    // Check if the login form is submitted
-    if (isset($_POST['login'])) {
-        // Retrieve the form data
-        $username = $_POST['username'];
-        $password = $_POST['password'];
 
-        // Perform your authentication logic here
-        // Example: you can check against a database or a predefined list of credentials
+session_start(); 
+// require_once "C:\wamp64\www\Les-Caisses-du-Seigneurs\src\Repo\connectdb.php";
 
-        // For demonstration purposes, let's assume the username is "admin" and the password is "password"
-        $validUsername = 'admin';
-        $validPassword = 'password';
 
-    //Faire un e boucle qui checkera si le username et me passeword sont dans la base de donnés
 
-        if ($username == $validUsername && $password == $validPassword) {
-            // Successful login
-            echo '<script>alert("Login successful!");</script>';
-            // Redirect to another page
-            header("Location: ../admin.html");
+$user = 'root';
+$password = 'HLyeafaW2ajD4Y';
+$database = 'lescaissesduseigneurs'; 
+
+$conn = new mysqli('127.0.0.1', $user, $password, $database, $port);
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+// Start a PHP session if you haven't already
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form inputs
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Prepare the SQL statement with placeholders
+    $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        // Bind parameters to the prepared statement
+        $stmt->bind_param("ss", $username, $password);
+
+        // Execute the prepared statement
+        $stmt->execute();
+
+        // Check if a matching row is found
+        $result = $stmt->get_result();
+        if ($result->num_rows == 1) {
+            // User authenticated successfully
+            $_SESSION['username'] = $username;
+            // Redirect to a logged-in page or perform any other necessary actions
+            header("Location: ../admin.php");
             exit();
         } else {
             // Invalid login credentials
@@ -26,5 +52,11 @@
             header("Location: ./login.html");
             exit();
         }
+
+        $stmt->close();
     }
-    ?>
+}
+?>
+<?php
+// $conn->close();
+?>
--- src/Repo/connectdb.php
+++ src/Repo/connectdb.php
@@ -1,65 +1,20 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-<body>
 
-<?php
-// Echo session variables that were set on previous page
-echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
-?>
-<?php
-/*
-* Change the value of $password if you have set a password on the root userid
-* Change NULL to port number to use DBMS other than the default using port 3306
-*
-*/
+// Il y a un bug, il faut séparer la connection, il faut qu'elle se fasse ici
+
 $user = 'root';
-$password = 'HLyeafaW2ajD4Y'; //To be completed if you have set a password to root
-$database = 'LesCaissesduSeigneurs'; //To be completed to connect to a database. The database must exist.
-$port = 3306; 
-$mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
+$password = 'HLyeafaW2ajD4Y';
+$database = 'lescaissesduseigneurs'; 
 
-//////////////////////////////////////////////////////////
+$conn = new mysqli('127.0.0.1', $user, $password, $database, $port);
 
 
-
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-}
-echo '<p>Connection OK '. $mysqli->host_info.'</p>';
-echo '<p>Server '.$mysqli->server_info.'</p>';
-echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
-$sql = "SELECT username, password FROM users";
-$result = $mysqli->query($sql);
-
-
-///////////////////////////////////////////////////////////////
-
-
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Username: " . $row["username"]. "<br>";
-  }
-} else {
-  echo "0 results";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    echo "CONNEXION";
-
-}
 $mysqli->close();
 ?>
-</body></html>
